1d75893edb1cc7261abbc7d68940db16679de8b2
mrericson <mrericson@mrericsons-MacBook-Pro.local>
2017-09-30 14:59:21 +0200

    Added PrimaireCollector Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PrimaireCollectorComponent } from './components/primaire-collector/primaire-collector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimaireCollectorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
